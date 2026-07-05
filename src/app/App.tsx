c6b65a7485d48767bdd25c1f5a3c27645c3416ef
Daniyal2513 <daniyal.shahid604@gmail.com>
2026-07-05 23:04:25 +0500

    change gap of 9px

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -423,7 +423,7 @@ export default function App() {
           {/* Grid */}
           <div
             className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-            style={{ gap: "5px",  }}
+            style={{ gap: "9px",  }}
           >
           {/* background: "rgba(255,255,255,0.05)" */}
             {currentCategory?.items.map(item => (
